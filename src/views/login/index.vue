ff9fa6db66a8bf7b646fe1ce0960fd70c99a8687
Zijing <2890951490@qq.com>
2024-06-28 16:58:38 +0800

    表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,18 @@
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="请输入手机号"></el-input>
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" placeholder="请输入密码" show-password />
+          </el-form-item>
+          <el-form-item prop="isAgree">
+            <el-checkbox v-model="loginForm.isAgree"> 用户平台使用协议 </el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-input placeholder="请输入密码"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-checkbox>
-              用户平台使用协议
-            </el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <el-button style="width:350px" type="primary">登录</el-button>
+            <el-button @click="login" style="width: 350px" type="primary">登录</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -27,7 +25,55 @@
 </template>
 <script>
 export default {
-  name : "Login"
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [{
+          required: true,
+          message: '请输入手机号',
+          trigger: 'blur'
+        }, {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '手机号格式不正确',
+          trigger: 'blur'
+        }],
+        password: [{
+          required: true,
+          message: '请输入密码',
+          trigger: 'blur'
+        }, {
+          min: 6,
+          max: 16,
+          message: '密码长度应该为6-16位之间',
+          trigger: 'blur'
+        }],
+        isAgree: [{
+          validator: (rule, value, callback) => {
+            // 校验规则
+            // 校验的值
+            // 函数
+            // callback() callback(new Error(错误信息))
+            value ? callback() : callback(new Error('您必须勾选用户协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.form.validate((isOK) => {
+        if (isOK) {
+          alert('校验通过')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -82,7 +128,7 @@ export default {
       }
     }
     .el-checkbox {
-      color:#606266;
+      color: #606266;
     }
   }
 }
